8283bf5268b46cdbec4abb4fbed7e4baba77e5b7
motevalian <rezamote@gmail.com>
2021-01-21 15:45:02 +0330

    refactoring payment output to file and console

--- src/main/java/ir/dotin/files/PaymentFileHandler.java
+++ src/main/java/ir/dotin/files/PaymentFileHandler.java
@@ -2,7 +2,8 @@ package ir.dotin.files;
 
 import ir.dotin.Main;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +13,6 @@ import static ir.dotin.business.DepositType.CREDITOR;
 import static ir.dotin.business.DepositType.DEBTOR;
 
 public class PaymentFileHandler {
-    // private static final String DEBTOR = "debtor";
-    // private static final String CREDITOR = "creditor";
     private static final int MIN_AMOUNT = 100;
     private static final int MAX_AMOUNT = 10000;
     private static final String PAYMENT_FILE_PATH = Main.FILE_PATH_PREFIX + "Payment.txt";
@@ -21,59 +20,33 @@ public class PaymentFileHandler {
 
     public static List<PaymentVO> createPaymentFile(String debtorDepositNumber, String creditorDepositNumberPrefix, int creditorCount) throws IOException, ClassNotFoundException {
         List<PaymentVO> paymentVOs = new ArrayList<>();
-        //  PaymentVO paymentVO=new PaymentVO();
         paymentVOs.add(new PaymentVO(DEBTOR, debtorDepositNumber, generateRandomAmount()));
         for (int i = 1; i <= creditorCount; i++) {
             paymentVOs.add(new PaymentVO(CREDITOR, creditorDepositNumberPrefix + i, generateRandomAmount()));
-
         }
         writePaymentRecordsToFile(paymentVOs);
-        // read and output serialize
-//----------------------------------------------------------
-        FileInputStream payIn = new FileInputStream(PAYMENT_FILE_PATH);
-        ObjectInputStream in = new ObjectInputStream(payIn);
-        // paymentVO = (PaymentVO) in.readObject();
-        paymentVOs.add((PaymentVO) in.readObject());
-        System.out.println(paymentVOs);
-        in.close();
-        payIn.close();
-//----------------------------------------------------------
-       /* FileReader payReader = new FileReader(PAYMENT_FILE_PATH);
-        BufferedReader pr = new BufferedReader(payReader);
-        String s;
-        while ((s = pr.readLine()) != null) {
-            System.out.println(s);
-        }
-        payReader.close();*/
+        printPaymentVOsToConsole(paymentVOs);
         return paymentVOs;
 
     }
 
-
     public static BigDecimal generateRandomAmount() {
-        BigDecimal rand = BigDecimal.valueOf(0);
-
-        rand = BigDecimal.valueOf(random.nextInt((MAX_AMOUNT - MIN_AMOUNT) + 1) + MIN_AMOUNT);
-        return rand;
+        return BigDecimal.valueOf(random.nextInt((MAX_AMOUNT - MIN_AMOUNT) + 1) + MIN_AMOUNT);
     }
 
+    private static void printPaymentVOsToConsole(List<PaymentVO> paymentVOs) {
+        System.out.println("========================= PAYMENTS =========================");
+        for (PaymentVO paymentVO : paymentVOs)
+            System.out.println(paymentVO.toString());
+        System.out.println("============================================================");
+    }
 
     private static void writePaymentRecordsToFile(List<PaymentVO> paymentVOs) throws IOException {
-//----------------------------
-//serialize
-        FileOutputStream pout = new FileOutputStream(PAYMENT_FILE_PATH);
-        ObjectOutputStream payOut = new ObjectOutputStream(pout);
-//----------------------------
-        //  PrintWriter printWriter = new PrintWriter(PAYMENT_FILE_PATH);
-        // FileWriter fileWriter = new FileWriter(PAYMENT_FILE_PATH, true);
+        PrintWriter printWriter = new PrintWriter(PAYMENT_FILE_PATH);
         for (PaymentVO paymentVO : paymentVOs) {
-            //  printWriter.println(paymentRecord.toString());
-            payOut.writeObject(paymentVO);
+            printWriter.println(paymentVO.toString());
         }
-
-        //  printWriter.close();
-        payOut.close();
+        printWriter.close();
     }
-
 
 }
